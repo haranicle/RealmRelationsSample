3a8e5fbd26bb13cec51f4888abb1b0620b2b005b
haranicle <haranicle@gmail.com>
2015-02-27 21:41:00 +0900

    ++ project

---
+++ RealmRelationsSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RealmRelationsSample
+//
+//  Created by haranicle on 2015/02/27.
+//  Copyright (c) 2015年 haranicle. All rights reserved.
+//
+
+import UIKit
+import Realm
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
